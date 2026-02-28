4ad28a027f99d2ce5997a35b74a10a0c54d59858
Aadi Jain <aadijain74174@gmail.com>
2026-02-28 20:07:03 +0530

    Update Simple_Interest.c

--- Simple_Interest.c
+++ Simple_Interest.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-  float principal, rate, time, year, month, days;
+  float principal, rate, mrate, time, year, month, days;
   printf("Welcome to simple interest caculatour\n");
   printf("Please enter the principal value: ");
   scanf("%f", &principal);
   printf("Now, enter the rate: ");
-  scanf("%f", &rate);
+  scanf("%f", &mrate);
   printf("Now, enter the year: ");
   scanf("%f", &year);
   printf("Now, enter the month: ");
@@ -14,6 +14,7 @@ int main()
   printf("Now, enter the days: ");
   scanf("%f", &days);
   
+  rate = (mrate*12);
   time = (year + (month/12.0) + (days/365.0));
   float Interest = (principal*rate*time)/100, Total = principal + Interest;
   printf("Simple ineterest is %f", Interest);
